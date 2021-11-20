ef5e2f1ff505394be477404a82f67b6d6fb9b763
otamm <oivatom@gmail.com>
2021-11-20 17:00:55 -0500

    basic package version

---
+++ setup.py
@@ -0,0 +1,43 @@
+# build with $python3 setup.py sdist bdist_wheel
+# Package publisher: $pip3 install twine
+# Sanity check with $python3 -m twine check dist/*
+# Upload to test registry with $python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# Upload to registry with $python3 -m twine upload dist/*
+# Local install $ python3 setup.py develop
+# some of these imports have redundant utilities, change later
+import os
+import sys
+import pathlib
+# appends child dir to PATH
+PRYTHON_SRC_PATH = os.getcwd() + "/prython_src"
+sys.path.insert(0, PRYTHON_SRC_PATH) # inserts $(pwd)/prython_src to current path
+import prython # import source code to have a single source of constants
+from setuptools import setup
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="prython",
+    version=prython.__version__,
+    description="Runtime microdebugger for Python, inspire by Ruby's pry gem.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=prython.__main_repo_url__,
+    author="Otavio Ehrenberger",
+    author_email="ehren.dev.mail@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["prython"],
+    include_package_data=False,
+    install_requires=["pathlib ; python_version<'3.4'"],
+    entry_points={
+        "console_scripts": [
+            "prython=prython.__main__:main",
+        ]
+    },
+)
